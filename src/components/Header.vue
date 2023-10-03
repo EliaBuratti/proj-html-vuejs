7bc7f6491c5ed43fb67240faa6e05a7d0bf717d9
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-03 12:11:44 +0200

    create navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,10 +6,54 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>headers section!</h1>
-        <hr>
-    </div>
+    <section id="eb_nav_section">
+        <nav class="navbar navbar-expand-lg bg-transparent">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="##">
+                    <img src="../assets/images/avada-bakery-logo-retina-200x97.png" alt="bakery logo">
+                </a>
+                <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto text-end">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##">shop</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##">about</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##">gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##">locations</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##">journal</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##">contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##">my account</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="##"><svg xmlns="http://www.w3.org/2000/svg" height="1.25em"
+                                    viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path
+                                        d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                                </svg></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
 </template>
 
 
